0481ab857c48e95aea389e344cf7cc88a3c984a6
Anushri <112797477+anu2112002@users.noreply.github.com>
2025-02-06 12:44:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    str = str.toLowerCase();
+    let characterCount = {};
+    for (let char of str) {
+        if (characterCount[char]) {
+            characterCount[char] += 1;
+        } else {
+            characterCount[char] = 1;
+        }
+    }
+    for (let char of str) {
+        if (characterCount[char] === 1) {
+            return char;
+        }
+    }
+    return null;
 }
+
+// Do not change the code below
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
